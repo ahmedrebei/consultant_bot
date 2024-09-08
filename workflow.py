f876532651acdaccd546d316920736f1f5db067a
Ahmed Rebei <arebei@concordia.ca>
2024-09-08 15:56:50 -0400

    change workflow with new agents

--- workflow.py
+++ workflow.py
@@ -4,49 +4,74 @@ from agents.retrieval_agent import RetrievalAgent
 
 from loguru import logger
 
+from agents.summarization_agent import SummarizationAgent
+
 
 class WorkFlow:
     def __init__(self):
         self.greeting_agent = GreetingAgent()
-        # output: "immigration", "education", "other" or "not related"
-
         self.retrieve_agent = None
         self.generator_agent = GeneratorAgent()
-        self.conversation_history = ""
+        self.summarization_agent = SummarizationAgent()
 
-    def update_conversation_history(self, query: str):
-        self.conversation_history += query
-        # self.conversation_history = self.conversation_history[:-1000]
-        
-    def delete_conversation_history(self):
-        self.conversation_history = ""
+        self.history_window_size = 3
+        self.conversation_history = []
+
+    def update_conversation_history(
+        self,
+        speaker: str,
+        conversation_item: str,
+    ):
+        self.conversation_history.append(speaker + ": " + conversation_item + "\n")
+        self.conversation_history = self.conversation_history[-self.history_window_size :]
 
     def run(self, query: str):
-        expertise = self.greeting_agent.greet(
+
+        expertise = self.greeting_agent.greet(query=query)
+        print(expertise)
+        conversation_summary = self.summarization_agent.summarize(
             query=query, conversation_history=self.conversation_history
         )
-        self.update_conversation_history(query=query)
 
         if expertise in ["immigration", "education"]:
+
             self.retrieve_agent = RetrievalAgent(expertise=expertise)
+
             search_results = self.retrieve_agent.execute(query=query, top_k=2)
             answer = self.generator_agent.generate_answer(
                 expertise=expertise,
+                query=query,
                 search_results=search_results,
-                conversation_history=self.conversation_history,
+                conversation_history=conversation_summary,
+            )
+
+            self.update_conversation_history(speaker="User", conversation_item=query)
+            self.update_conversation_history(
+                speaker="Bot Assistant", conversation_item=answer
             )
             return answer
         else:
             answer = self.generator_agent.generate_answer(
                 expertise=expertise,
+                query=query,
                 search_results=[],
-                conversation_history=self.conversation_history,
+                conversation_history=conversation_summary,
+            )
+
+            self.update_conversation_history(speaker="User", conversation_item=query)
+            self.update_conversation_history(
+                speaker="Bot Assistant", conversation_item=answer
             )
             return answer
 
 
 if __name__ == "__main__":
     workflow = WorkFlow()
-    query="I want to add my spouse to my permanent residence application in Quebec"
+    query = "I want to study in Toronto. What should we do?"
     result = workflow.run(query=query)
     print(result)
+    print("-------------------")
+    query = "I want to study in a Cegep in Montreal. What are the requirements?"
+    result = workflow.run(query=query)
+    print(result)
+    print("-------------------")
